d2b4db25c6e424960486b2346abab1dccbeecd82
Yevgeny Menaker <jeka.menaker@gmail.com>
2022-11-11 00:21:17 -0800

    few more tests

--- hh/scripts/addLiquidity.ts
+++ hh/scripts/addLiquidity.ts
@@ -4,8 +4,9 @@ import UniswapV2 from "./UniswapV2.json";
 const FNX_ADDRESS = "0x69c0AfB1Fa6D929C55bEb251Ff19dA3900777621"
 const MATIC_ADDRESS = "0x0000000000000000000000000000000000001010"
 const UNISWAP_ROUTER_ADDRESS = "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff"
+const UNISWAP_FACTORY_ADDRESS = "0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32"
 
-const GAS_LIMIT: number = 1000000;
+const GAS_LIMIT: number = 10000000;
 
 
 async function getUniswapV2Contract() {
@@ -23,11 +24,11 @@ async function main() {
     const FNX = await ethers.getContractFactory("Token");
     const fnx = await FNX.attach("0x69c0AfB1Fa6D929C55bEb251Ff19dA3900777621");
     const uniswapV2 = await getUniswapV2Contract();
-    const allowance = await fnx.allowance(signer.address, UNISWAP_ROUTER_ADDRESS);
+    const allowance = await fnx.allowance(signer.address, UNISWAP_FACTORY_ADDRESS);
     console.log(`FNX allowance: ${allowance}`);
     // const wethAddress = await uniswapV2.WETH({gasLimit: GAS_LIMIT});
     // console.log(`WETH address: ${wethAddress}`);
-    // fnx.approve(UNISWAP_ROUTER_ADDRESS, 10000);
+    await fnx.approve(UNISWAP_FACTORY_ADDRESS, 10000);
 
     const res = await uniswapV2.addLiquidity(
         MATIC_ADDRESS,
