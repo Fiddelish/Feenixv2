d24cb7741b58b7d5a15869dabc735e677668e134
Quantotto Inc <quantottoinc@gmail.com>
2023-02-27 10:34:32 +0200

    adding nginx

--- rwo/deployment/create.sh
+++ rwo/deployment/create.sh
@@ -3,6 +3,9 @@ set -ex
 
 sudo rm -rf .data
 source .env
+cd nginx
+docker build . -t rwonginx:dev
+cd ..
 docker network create --subnet ${RWO_NETWORK_SUBNET} rwo_network
 echo "Starting hugs services..."
 docker-compose --env-file .env -f docker-compose.yml up -d
