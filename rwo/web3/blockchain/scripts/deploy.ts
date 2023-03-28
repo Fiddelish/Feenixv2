50b3a90e8ef71a259225c3ec99235b5328ac27b9
Yev M <quantottoinc@gmail.com>
2023-03-28 15:59:59 +0000

    add deploy

---
+++ rwo/web3/blockchain/scripts/deploy.ts
@@ -0,0 +1,26 @@
+import { ethers } from "hardhat";
+
+// const GAS_LIMIT: number = 1000000;
+
+async function main() {
+  console.log("Deploying token");
+  const randomTokenFactory = await ethers.getContractFactory("RandomToken");
+  const randomToken = await randomTokenFactory.deploy();
+  await randomToken.deployed();
+  console.log(`Token contract deployed to ${randomToken.address}`);
+
+  console.log("Deploying Crypto Store");
+  const cryptoStoreFactory = await ethers.getContractFactory("CryptoStore");
+  const cryptoStore = await cryptoStoreFactory.deploy();
+  await cryptoStore.deployed();
+  console.log(`Crypto Store contract deployed to ${cryptoStore.address}`);
+  await cryptoStore.SetTokenAddress(randomToken.address);
+  console.log("Crypto Store: token address set");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
