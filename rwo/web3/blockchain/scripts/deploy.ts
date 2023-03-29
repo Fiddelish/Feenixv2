45ae441a8f35e6b82464762e013f606263f5c5ec
Yev M <quantottoinc@gmail.com>
2023-03-29 20:17:13 +0000

    extend contract deployment script

--- rwo/web3/blockchain/scripts/deploy.ts
+++ rwo/web3/blockchain/scripts/deploy.ts
@@ -3,7 +3,8 @@ import { ethers } from "hardhat";
 // const GAS_LIMIT: number = 1000000;
 
 async function main() {
-  console.log("Deploying token");
+  const [owner, other, dev1, dev2, dev3, rwo, fnx] = await ethers.getSigners();
+  console.log(`Deploying token; owner: ${owner.address}`);
   const randomTokenFactory = await ethers.getContractFactory("RandomToken");
   const randomToken = await randomTokenFactory.deploy();
   await randomToken.deployed();
@@ -28,6 +29,21 @@ async function main() {
     ethers.utils.parseUnits("21.50", decimals)
   );
   console.log("Added product 2");
+  await cryptoStore.UpdateDev1Wallet(dev1.address);
+  console.log(`Updated dev1 wallet`);
+  await cryptoStore.UpdateDev2Wallet(dev2.address);
+  console.log(`Updated dev2 wallet`);
+  await cryptoStore.UpdateDev3Wallet(dev3.address);
+  console.log(`Updated dev3 wallet`);
+  await cryptoStore.UpdateRWOWallet(rwo.address);
+  console.log(`Updated rwo wallet`);
+  await cryptoStore.UpdateFNXWallet(fnx.address);
+  console.log(`Updated fnx wallet`);
+  randomToken.transfer(
+    other.address,
+    ethers.utils.parseUnits("1000000", decimals)
+  );
+  console.log(`Transferred 1000000 tokens to ${other.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
