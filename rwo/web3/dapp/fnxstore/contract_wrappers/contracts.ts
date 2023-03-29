77ac91fc5dcbb8a15d325ab9a6da948320441c1c
Yev M <quantottoinc@gmail.com>
2023-03-29 20:17:38 +0000

    update contract addresses

--- rwo/web3/dapp/fnxstore/contract_wrappers/contracts.ts
+++ rwo/web3/dapp/fnxstore/contract_wrappers/contracts.ts
@@ -2,8 +2,8 @@ import { ethers } from "ethers";
 import CryptoStore from "../abi/contracts/CryptoStore.sol/CryptoStore.json";
 import RandomToken from "../abi/contracts/RandomToken.sol/RandomToken.json";
 
-export const CRYPTO_STORE_CONTRACT: string = "0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82";
-export const TOKEN_CONTRACT: string = "0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0";
+export const CRYPTO_STORE_CONTRACT: string = "0xE6E340D132b5f46d1e472DebcD681B2aBc16e57E";
+export const TOKEN_CONTRACT: string = "0x67d269191c92Caf3cD7723F116c85e6E9bf55933";
 
 export function getCryptoStoreContract() {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
