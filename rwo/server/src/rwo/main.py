436e80eb6c63c216693d9140529e8c14b7d50275
zarganum <unpublished@nowhere.org>
2023-04-03 21:08:47 +0400

    add router, set UTC, update version

--- rwo/server/src/rwo/main.py
+++ rwo/server/src/rwo/main.py
@@ -7,17 +7,13 @@ import uvicorn
 from .api.routers import (
     product,
     order,
+    notification,
 )
 
-RWO_API_SERVER = os.getenv(
-    "RWO_API_SERVER",
-    "http://localhost:5000"
-)
-EXTRA_CORS_ORIGINS = os.getenv(
-    "EXTRA_CORS_ORIGINS",
-    ""
-).split(",")
+RWO_API_SERVER = os.getenv("RWO_API_SERVER", "http://localhost:5000")
+EXTRA_CORS_ORIGINS = os.getenv("EXTRA_CORS_ORIGINS", "").split(",")
 CORS_ALLOWED_ORIGINS = [RWO_API_SERVER] + EXTRA_CORS_ORIGINS
+
 
 def build_app():
     app = FastAPI(
@@ -34,19 +30,22 @@ def build_app():
     )
     app.include_router(product.router)
     app.include_router(order.router)
+    app.include_router(notification.router)
     openapi_schema = get_openapi(
-       title="RWO Product API",
-       version="2023.3",
-       routes=app.routes,
+        title="RWO Product API",
+        version="2023.3",
+        routes=app.routes,
     )
     openapi_schema["info"] = {
-        "title" : "RWO API",
-        "version" : "2022.10",
-        "description" : "RWO Product API"
+        "title": "RWO API",
+        "version": "2023.3",
+        "description": "RWO Product API",
     }
     app.openapi_schema = openapi_schema
     app.openapi = lambda: openapi_schema
     return app
 
+
 def run():
+    os.environ["TZ"] = "UTC"
     uvicorn.run(build_app(), host="0.0.0.0", port=5000)
