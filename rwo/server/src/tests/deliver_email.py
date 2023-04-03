7b42de1b4d2ac39604ea689492c94d386055dfc3
zarganum <unpublished@nowhere.org>
2023-04-03 21:10:20 +0400

    order email notification worker

---
+++ rwo/server/src/tests/deliver_email.py
@@ -0,0 +1,77 @@
+import asyncio
+import aiosmtplib
+import json
+from datetime import datetime
+
+from rwo_py_sdk import ApiClient as RWOApiClient
+from rwo_py_sdk.configuration import Configuration as RWOConfiguration
+from rwo_py_sdk.apis import NotificationApi
+from rwo_py_sdk.models import UpdateNDSRequest
+from rwo.api.models import NotificationChannel, NotificationSubscriber
+
+
+api = NotificationApi(
+    RWOApiClient(configuration=RWOConfiguration(host="http://localhost:5000"))
+)
+
+
+def dt2iso(o):
+    if isinstance(o, datetime):
+        return o.isoformat()
+
+
+async def email_notify(subscriber: NotificationSubscriber):
+    global api
+    while True:
+        items = api.get_notifications_pending(
+            subscriber=subscriber,
+            channel=NotificationChannel.email.value,
+        )
+        if len(items) > 0:
+            smtp_client = aiosmtplib.SMTP(hostname="localhost", port=2525)
+            await smtp_client.connect()
+            # await smtp_client.starttls()
+            # await smtp_client.login()
+            for _ in items:
+                message = json.dumps(_.to_dict(), indent=2, default=dt2iso)
+                successful: bool = None
+                try:
+                    id, response = await smtp_client.sendmail(
+                        "", _.order_email, message
+                    )
+                    successful = True
+                except Exception as e:
+                    successful = False
+                    response = e.__str__()
+                finally:
+                    api.update_delivery_status(
+                        _.id,
+                        UpdateNDSRequest(
+                            successful=successful,
+                            report=response,
+                        ),
+                    )
+
+            await smtp_client.quit()
+        await asyncio.sleep(5)
+
+
+async def notify_user():
+    while True:
+        await asyncio.sleep(1)
+
+
+async def main():
+    tasks = [
+        asyncio.create_task(email_notify(NotificationSubscriber.admin.value)),
+        asyncio.create_task(email_notify(NotificationSubscriber.user.value)),
+    ]
+    try:
+        await asyncio.gather(*tasks)
+    except KeyboardInterrupt:
+        print("Keyboard interrupt")
+        for _ in tasks:
+            _.cancel()
+
+
+asyncio.run(main())
