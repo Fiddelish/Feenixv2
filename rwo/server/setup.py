ea603e6b3be04fa5516f046530525da126543899
zarganum <unpublished@nowhere.org>
2023-04-07 17:51:42 +0400

    rwonotify entry point

--- rwo/server/setup.py
+++ rwo/server/setup.py
@@ -1,19 +1,18 @@
 # coding: utf-8
 
 
-
 from setuptools import setup, find_packages  # noqa: H301
 import os
 
 NAME = "rwo"
 
-def get_version():
-    version = '0.0.0'
-    print(f'Current directory: [{os.getcwd()}]')
-    with open(f'VERSION.py', 'r') as f:
-        version = f.readline().rstrip().split('=')[1].replace('"', '')
-    return version
 
+def get_version():
+    version = "0.0.0"
+    print(f"Current directory: [{os.getcwd()}]")
+    with open(f"VERSION.py", "r") as f:
+        version = f.readline().rstrip().split("=")[1].replace('"', "")
+    return version
 
 
 # To install the library, run the following
@@ -43,5 +42,6 @@ setup(
     entry_points="""
         [console_scripts]
         rwoapi=rwo.main:run
-    """
+        rwonotifier=rwo.manager.notifier:run
+    """,
 )
