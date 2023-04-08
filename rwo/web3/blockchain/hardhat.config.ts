e103c74796620d2d45f57197155e6c36744093d1
Yev <yev@quantotto.io>
2023-04-08 20:19:01 +0300

    add mumbai network

--- rwo/web3/blockchain/hardhat.config.ts
+++ rwo/web3/blockchain/hardhat.config.ts
@@ -25,6 +25,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+/////////////////////////////////////////////////////
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "8MQ8BytdpPqO5kqDfOnO0uzEQZtYlz2a";
+
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -47,6 +54,11 @@ const config: HardhatUserConfig = {
     },
     docker: {
       url: "http://hardhat:8545",
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
