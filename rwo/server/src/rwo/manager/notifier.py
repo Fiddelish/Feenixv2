fd8829a3489f01524aa074b70d3a8a7b020e23d3
zarganum <unpublished@nowhere.org>
2023-04-09 22:47:39 +0400

    jinja2

--- rwo/server/src/rwo/manager/notifier.py
+++ rwo/server/src/rwo/manager/notifier.py
@@ -1,40 +1,83 @@
-import asyncio
-import aiosmtplib
-from aiosmtplib import errors as SMTPErrors
-from email.mime.text import MIMEText
-
 import json
 import os
 import aioredis
+import asyncio
+import aiosmtplib
+import traceback
+from aiosmtplib import errors as SMTPErrors
+
+from email.mime.text import MIMEText
+from jinja2 import Environment, BaseLoader
+import textwrap
+
 from rwo.common import logging as rwo_logging
-from datetime import datetime
+from rwo.common.utils import datetime_to_iso, mask_email, env_to_bool
 
 from rwo_py_sdk import ApiClient as RWOApiClient
 from rwo_py_sdk.configuration import Configuration as RWOConfiguration
 from rwo_py_sdk.apis import NotificationApi
 from rwo_py_sdk.models import UpdateNDSRequest
 
-API_SERVER = os.getenv("RWO_API_SERVER", "http://localhost:5000")
+DEVMODE = env_to_bool("RWO_DEVMODE")
+API_URL = os.getenv("RWO_API_URL", "http://localhost:5000")
 REDIS_URL = os.getenv("RWO_REDIS_URL")
 SMTP_HOST = os.getenv("RWO_SMTP_HOST", "localhost")
 SMTP_PORT = os.getenv("RWO_SMTP_PORT", "25")
+SMTP_SENDER = os.getenv("RWO_SMTP_SEMDER", "")
+SMTP_STARTTLS = env_to_bool("RWO_SMTP_STARTTLS")
+SMTP_USER = os.getenv("RWO_SMTP_USER")
+SMTP_PASSWORD = os.getenv("RWO_SMTP_PASSWORD")
+
 QUEUE_GUARD_TIMEOUT = 60 * 30
 EMAIL_RETRY_TIME = 60 * 5
 CHUNK_SIZE = 50
 
-api = NotificationApi(RWOApiClient(configuration=RWOConfiguration(host=API_SERVER)))
-logger = rwo_logging.init_logger("notifier.log", rwo_logging.DEBUG, "notifier")
+jinja2_email_tmpl = {
+    "user": {
+        "subject": "RWO Order Status: {{status}}",
+        "body": textwrap.dedent(
+            """
+            Dear User,
+
+            Your order {{id}} status is {{ status }}
+            Created (UTC): {{ created_at }}
+            Transaction ID: {{tx_id}}
+
+            kind regards
+            """
+        ),
+    },
+    "admin": {
+        "subject": "RWO ADMIN order {{id}} status {{status}}",
+        "body": textwrap.dedent(
+            """
+            Dear Admin,
+
+            Order id: {{ id }}
+            Order status: {{ status }}
+            Created (UTC): {{ created_at }}
+            Updated (UTC): {{ updated_at }}
+
+            User email: {{email}}
+            User wallet: {{wallet}}
+            Product ID: {{product_id}}
+            Quantity: {{quantity}}
+            Transaction ID: {{tx_id}}
+            Transaction hash: {{tx_hash}}
+
+            Admin Token: {{token}}
+
+            kind regards
+            """
+        ),
+    },
+}
+
+api = NotificationApi(RWOApiClient(configuration=RWOConfiguration(host=API_URL)))
+logger = rwo_logging.init_logger(
+    "notifier.log", rwo_logging.DEBUG if DEVMODE else rwo_logging.INFO, "notifier"
+)
 redis_pool = None
-
-
-def dt2iso(o):
-    if isinstance(o, datetime):
-        return o.isoformat()
-
-
-def mask_email(e):
-    # TODO mask unless in dev mode
-    return e
 
 
 async def email_notify_thread(subscriber: str):
@@ -75,20 +118,36 @@ async def email_notify_thread(subscriber: str):
             logger.debug(f"trying SMTP {SMTP_HOST}:{SMTP_PORT}")
             smtp_client = aiosmtplib.SMTP(hostname=SMTP_HOST, port=SMTP_PORT)
             await smtp_client.connect()
-            # await smtp_client.starttls()
-            # await smtp_client.login()
+            if SMTP_STARTTLS:
+                logger.debug(f"SMTP STARTTLS enforced")
+                await smtp_client.starttls()
+            if SMTP_USER:
+                logger.debug(f"SMTP authentication enabled with {SMTP_USER}")
+                await smtp_client.login(SMTP_USER, SMTP_PASSWORD)
             for _ in notifications:
                 successful: bool = None
 
-                message = MIMEText(json.dumps(_.data, indent=2, default=dt2iso))
+                # logger.debug(f"data[id]={_.data['id']}")
+                # message = MIMEText(
+                #     json.dumps(_.data, indent=2, default=datetime_to_iso)
+                # )
+                message = MIMEText(
+                    Environment(loader=BaseLoader())
+                    .from_string(jinja2_email_tmpl[subscriber]["body"])
+                    .render(_.data)
+                )
                 message["From"] = "notify-DAEMON <>"
                 message["To"] = _.recipient
-                message["Subject"] = f"RWO {subscriber.capitalize()} notification"
+                message["Subject"] = (
+                    Environment(loader=BaseLoader())
+                    .from_string(jinja2_email_tmpl[subscriber]["subject"])
+                    .render(_.data)
+                )
 
                 try:
-                    logger.debug(f"sendmail rcpt to {mask_email(_.recipient)}")
+                    logger.info(f"sendmail to {mask_email(_.recipient)}")
                     id, response = await smtp_client.sendmail(
-                        "", _.recipient, message.as_bytes()
+                        SMTP_SENDER, _.recipient, message.as_bytes()
                     )
                     logger.debug(f"successful sendmail id={id}, response={response}")
                     successful = True
@@ -132,7 +191,7 @@ async def email_notify_thread(subscriber: str):
         try:
             await process_pending_items(subscriber)
         except Exception as e:
-            logger.error(f"error processing: {e}")
+            logger.error(f"error processing: {e}\n{traceback.format_exc()}")
         return
 
     redis = await aioredis.Redis(
@@ -165,6 +224,9 @@ async def email_notify_thread(subscriber: str):
 async def main():
     global logger
     global redis_pool
+
+    if DEVMODE:
+        logger.warn(f"running in DEVMODE")
 
     if not REDIS_URL is None:
         logger.debug(f"using {REDIS_URL}")
