f691581a98e766bdbdc16dc99edbecf609247e76
zarganum <unpublished@nowhere.org>
2023-04-11 12:15:17 +0400

    fancy price

---
+++ rwo/web3/dapp/fnxstore/components/price.tsx
@@ -0,0 +1,9 @@
+export function FancyPrice({ price, currency }: { price: string; currency: string }) {
+    const [d, c] = price.split(".");
+    return (
+        <span>
+            {d}
+            <span className="align-text-top text-xs">.{c}</span> {currency}
+        </span>
+    );
+}
