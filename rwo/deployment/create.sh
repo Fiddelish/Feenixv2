14db581ecf1f9095481fe8cdcfda820be36621eb
Yev M <quantottoinc@gmail.com>
2023-04-12 13:48:28 +0000

    remove rwonotifier port

--- rwo/deployment/create.sh
+++ rwo/deployment/create.sh
@@ -6,7 +6,7 @@ rm -rf certs/*
 # mkdir -p $SSL_CERTS_FOLDER $CERTBOT_FOLDER $ACME_HTTP01_FOLDER
 source .env
 [[ -d venv ]] && source venv/bin/activate
-python3 gencerts.py --common-name ${COMMON_NAME} --server-ip ${SERVER_IP}
+# python3 gencerts.py --common-name ${COMMON_NAME} --server-ip ${SERVER_IP}
 docker network create --subnet ${RWO_NETWORK_SUBNET} rwo_network
 echo "Starting hugs services..."
 docker-compose --env-file .env -f docker-compose.yml up -d
