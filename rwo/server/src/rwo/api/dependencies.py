bcdd8529546ce5f4cb6eb805bf37bec621ac8ab4
Yev M <quantottoinc@gmail.com>
2023-04-12 14:44:33 +0000

    fix redis init

--- rwo/server/src/rwo/api/dependencies.py
+++ rwo/server/src/rwo/api/dependencies.py
@@ -2,9 +2,9 @@ from ..db.database import DBSession
 import aioredis
 import os
 
-REDIS_URL = os.getenv("RWO_REDIS_URL")
+REDIS_URL = os.getenv("RWO_REDIS_URL", "redis://redis:6379")
 
-redis_pool = aioredis.ConnectionPool.from_url(REDIS_URL, decode_responses=True)
+redis_pool = None
 
 def get_db_session():
     print("Getting DB Session")
@@ -17,6 +17,9 @@ def get_db_session():
             db_session.close()
 
 async def get_redis():
+    global redis_pool
+    if not redis_pool:
+        redis_pool = aioredis.ConnectionPool.from_url(REDIS_URL, decode_responses=True)
     redis = await aioredis.Redis(
         connection_pool=redis_pool, encoding="utf-8", decode_responses=True
     )
