8eb9e73e6ce3514635aab4f3ba9e9dbe65cb96ce
zarganum <unpublished@nowhere.org>
2023-04-16 21:23:50 +0400

    product naive filtering by name

--- rwo/web3/dapp/fnxstore/components/product_list.tsx
+++ rwo/web3/dapp/fnxstore/components/product_list.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useWeb3React } from "@web3-react/core";
 import ProductCard from "./product_card";
+import { FilterState, initialFilterState, filterState$ } from "./product_filter";
 import { getProductApi } from "./api/product";
 import { Product } from "rwo_ts_sdk";
 
 export default function ProductList() {
     const [products, setProducts] = useState<Product[]>([]);
     const [isLoading, setLoading] = useState(false);
+    const [filter, setFilter] = useState(initialFilterState);
     const { active } = useWeb3React();
     useEffect(() => {
         setLoading(true);
@@ -15,11 +17,21 @@ export default function ProductList() {
             setProducts(value.data);
             setLoading(false);
         });
+        const filterSub = filterState$.subscribe((filterState) => {
+            setFilter(filterState);
+        });
+        return () => filterSub.unsubscribe();
     }, []);
+
+    if (isLoading) return <p>Loading...</p>;
+
     return (
         <div className="flex flex-wrap place-content-evenly">
-            {!isLoading && products.map((p) => <ProductCard key={p.id} product={p} />)}
-            {isLoading && <p>Loading...</p>}
+            {products
+                .filter((p) => p.name.toLowerCase().includes(filter.queryString.toLowerCase()))
+                .map((p) => (
+                    <ProductCard key={p.id} product={p} />
+                ))}
         </div>
     );
 }
